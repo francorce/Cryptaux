1bdaaca796d12782b2549c0c8afdc7c3681b98e2
Krypsio1 <71757403+Krypsio1@users.noreply.github.com>
2021-05-04 17:44:33 +0200

    ajout matteo

--- cryptaux/src/Controller/Admin/DashboardController.php
+++ cryptaux/src/Controller/Admin/DashboardController.php
@@ -10,9 +10,6 @@ use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use App\Entity\Page;
-use App\Entity\User;
-use App\Entity\Publication;
 
 
 class DashboardController extends AbstractDashboardController 
@@ -24,9 +21,7 @@ class DashboardController extends AbstractDashboardController
     {
         $routeBuilder = $this->get(AdminUrlGenerator::class);
 
-        return $this->redirect($routeBuilder->setController(PageCrudController::class)->generateUrl());
         return $this->redirect($routeBuilder->setController(UserCrudController::class)->generateUrl());
-        return $this->redirect($routeBuilder->setController(Publication::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
@@ -39,9 +34,7 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Page', 'fas fa-list', Page::class);
-        yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
-        yield MenuItem::linkToCrud('Publication', 'fas fa-list', Publication::class);
+        //yield MenuItem::linkToCrud('The Label', 'fas fa-list', User::class);
     }
 
 
--- cryptaux/src/Controller/RecupController.php
+++ cryptaux/src/Controller/RecupController.php
@@ -110,9 +110,7 @@ class RecupController extends AbstractController
         curl_setopt($curl14, CURLOPT_SSL_VERIFYPEER, 0);
         $return14 = curl_exec($curl14);
         curl_close($curl14);
-
-    
-
+        
         return $this->render('recup/index.html.twig', 
         array(
         'BTC' => json_decode($return),
